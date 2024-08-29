80710e2db9aa90b2530ef11a90dcc6bf7c993d59
Douglas.Y <siyuan.yang2@uq.net.au>
2024-08-30 06:01:00 +1000

    fix: the excede of cdk cloudformation size problem

--- src/cdk/createApiGateWay.ts
+++ src/cdk/createApiGateWay.ts
@@ -13,16 +13,22 @@ dotenv.config();
 
 export const createApiGateWay = (stack: Cdk.Stack) => {
   // lambda for http
-  const lambda = new NodejsFunction(stack, "ChatHttpLambda", {
+  const userLambda = new NodejsFunction(stack, "ChatUserLambda", {
     entry: join("./.build/chat.js"),
     handler: "httpHandler",
     runtime: Lambda.Runtime.NODEJS_18_X,
   });
 
-  lambda.addToRolePolicy(
+  userLambda.addToRolePolicy(
     new PolicyStatement({
       effect: Effect.ALLOW,
-      actions: ["dynamodb:*"],
+      actions: [
+        "dynamodb:GetItem",
+        "dynamodb:PutItem",
+        "dynamodb:DeleteItem",
+        "dynamodb:Query",
+        "dynamodb:Scan",
+      ],
       resources: [
         `${process.env.CHAT_TABLE_ARN as string}`,
         `${process.env.CHAT_TABLE_ARN as string}/index/*`, // for GISs
@@ -30,8 +36,60 @@ export const createApiGateWay = (stack: Cdk.Stack) => {
     })
   );
 
-  const api = new Apigateway.RestApi(stack, "ChatApi", {
-    restApiName: "Chat Service",
+  const groupLambda = new NodejsFunction(stack, "ChatGroupLambda", {
+    entry: join("./.build/chat.js"),
+    handler: "httpHandler",
+    runtime: Lambda.Runtime.NODEJS_18_X,
+  });
+
+  groupLambda.addToRolePolicy(
+    new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: [
+        "dynamodb:GetItem",
+        "dynamodb:PutItem",
+        "dynamodb:DeleteItem",
+        "dynamodb:Query",
+        "dynamodb:Scan",
+      ],
+      resources: [
+        `${process.env.CHAT_TABLE_ARN as string}`,
+        `${process.env.CHAT_TABLE_ARN as string}/index/*`, // for GISs
+      ],
+    })
+  );
+
+  const threadLambda = new NodejsFunction(stack, "ChatThreadLambda", {
+    entry: join("./.build/chat.js"),
+    handler: "httpHandler",
+    runtime: Lambda.Runtime.NODEJS_18_X,
+  });
+
+  threadLambda.addToRolePolicy(
+    new PolicyStatement({
+      effect: Effect.ALLOW,
+      actions: [
+        "dynamodb:GetItem",
+        "dynamodb:PutItem",
+        "dynamodb:DeleteItem",
+        "dynamodb:Query",
+        "dynamodb:Scan",
+      ],
+      resources: [
+        `${process.env.CHAT_TABLE_ARN as string}`,
+        `${process.env.CHAT_TABLE_ARN as string}/index/*`, // for GISs
+      ],
+    })
+  );
+
+  const userApi = new Apigateway.RestApi(stack, "ChatUserApi", {
+    restApiName: "Chat User Service",
+  });
+  const groupApi = new Apigateway.RestApi(stack, "ChatGroupApi", {
+    restApiName: "Chat Group Service",
+  });
+  const threadApi = new Apigateway.RestApi(stack, "ChatThreadApi", {
+    restApiName: "Chat Thread Service",
   });
 
   // lambda for websocket
@@ -45,9 +103,15 @@ export const createApiGateWay = (stack: Cdk.Stack) => {
   connectionLambda.addToRolePolicy(
     new PolicyStatement({
       effect: Effect.ALLOW,
-      actions: ["dynamodb:*"],
+      actions: [
+        "dynamodb:GetItem",
+        "dynamodb:PutItem",
+        "dynamodb:DeleteItem",
+        "dynamodb:Query",
+        "dynamodb:Scan",
+      ],
       resources: [
-        `${process.env.CHAT_API_ARN as string}/index/*`,
+        `${process.env.CHAT_TABLE_ARN as string}/index/*`,
         `${process.env.CHAT_TABLE_ARN as string}`,
       ],
     })
@@ -62,7 +126,13 @@ export const createApiGateWay = (stack: Cdk.Stack) => {
   disconnectLambda.addToRolePolicy(
     new PolicyStatement({
       effect: Effect.ALLOW,
-      actions: ["dynamodb:*"],
+      actions: [
+        "dynamodb:GetItem",
+        "dynamodb:PutItem",
+        "dynamodb:DeleteItem",
+        "dynamodb:Query",
+        "dynamodb:Scan",
+      ],
       resources: [
         `${process.env.CHAT_TABLE_ARN as string}`,
         `${process.env.CHAT_TABLE_ARN as string}/index/*`,
@@ -79,7 +149,13 @@ export const createApiGateWay = (stack: Cdk.Stack) => {
   defaultLambda.addToRolePolicy(
     new PolicyStatement({
       effect: Effect.ALLOW,
-      actions: ["dynamodb:*"],
+      actions: [
+        "dynamodb:GetItem",
+        "dynamodb:PutItem",
+        "dynamodb:DeleteItem",
+        "dynamodb:Query",
+        "dynamodb:Scan",
+      ],
       resources: [
         `${process.env.CHAT_TABLE_ARN as string}`,
         `${process.env.CHAT_TABLE_ARN as string}/index/*`,
@@ -90,13 +166,13 @@ export const createApiGateWay = (stack: Cdk.Stack) => {
   defaultLambda.addToRolePolicy(
     new PolicyStatement({
       effect: Effect.ALLOW,
-      actions: ["execute-api:*"],
+      actions: ["execute-api:Invoke", "execute-api:ManageConnections"],
       resources: [`${process.env.API_ARN as string}`],
     })
   );
 
-  const websocketApi = new apigatewayv2.WebSocketApi(stack, "ChatApi", {
-    apiName: "ChatApi",
+  const websocketApi = new apigatewayv2.WebSocketApi(stack, "WwsChatApi", {
+    apiName: "WwsChatApi",
     connectRouteOptions: {
       integration: new WebSocketLambdaIntegration(
         "connectionIntegration",
@@ -130,101 +206,113 @@ export const createApiGateWay = (stack: Cdk.Stack) => {
   });
 
   // /users
-  const userResource = api.root.addResource("users");
+  const userResource = userApi.root.addResource("users");
   const userResourceMethods = ["OPTIONS", "GET"];
   for (const method of userResourceMethods) {
-    userResource.addMethod(method, new Apigateway.LambdaIntegration(lambda));
+    userResource.addMethod(
+      method,
+      new Apigateway.LambdaIntegration(userLambda)
+    );
   }
 
   // /users/register
   const register = userResource.addResource("register");
   const registerMethods = ["POST", "OPTIONS"];
   for (const method of registerMethods) {
-    register.addMethod(method, new Apigateway.LambdaIntegration(lambda));
+    register.addMethod(method, new Apigateway.LambdaIntegration(userLambda));
   }
 
   // /users/login
   const login = userResource.addResource("login");
   const loginMethods = ["POST", "OPTIONS"];
   for (const method of loginMethods) {
-    login.addMethod(method, new Apigateway.LambdaIntegration(lambda));
+    login.addMethod(method, new Apigateway.LambdaIntegration(userLambda));
   }
 
   // /users/{userId}
   const user = userResource.addResource("{userId}");
   const userMethods = ["OPTIONS", "GET", "PUT", "DELETE"];
   for (const method of userMethods) {
-    user.addMethod(method, new Apigateway.LambdaIntegration(lambda));
+    user.addMethod(method, new Apigateway.LambdaIntegration(userLambda));
   }
 
   // /users/{userId}/password
   const password = user.addResource("password");
   const passwordMethods = ["PUT", "OPTIONS"];
   for (const method of passwordMethods) {
-    password.addMethod(method, new Apigateway.LambdaIntegration(lambda));
+    password.addMethod(method, new Apigateway.LambdaIntegration(userLambda));
   }
 
   // /users/{userId}/groups get users' groups
   const groups = user.addResource("groups");
   const groupsMethods = ["GET", "OPTIONS"];
   for (const method of groupsMethods) {
-    groups.addMethod(method, new Apigateway.LambdaIntegration(lambda));
+    groups.addMethod(method, new Apigateway.LambdaIntegration(userLambda));
   }
 
   // /groups create group by user
-  const groupResource = api.root.addResource("groups");
+  const groupResource = groupApi.root.addResource("groups");
   const groupResourceMethods = ["POST", "GET", "OPTIONS"];
   for (const method of groupResourceMethods) {
-    groupResource.addMethod(method, new Apigateway.LambdaIntegration(lambda));
+    groupResource.addMethod(
+      method,
+      new Apigateway.LambdaIntegration(groupLambda)
+    );
   }
 
   // /groups/{groupId}
   const group = groupResource.addResource("{groupId}");
   const groupMethods = ["GET", "OPTIONS"];
   for (const method of groupMethods) {
-    group.addMethod(method, new Apigateway.LambdaIntegration(lambda));
+    group.addMethod(method, new Apigateway.LambdaIntegration(groupLambda));
   }
 
   // /groups/{groupId}/users
   const groupUsers = group.addResource("users");
   const groupUsersMethods = ["GET", "POST", "OPTIONS"];
   for (const method of groupUsersMethods) {
-    groupUsers.addMethod(method, new Apigateway.LambdaIntegration(lambda));
+    groupUsers.addMethod(method, new Apigateway.LambdaIntegration(groupLambda));
   }
 
   // /groups/{groupId}/threads
   const groupThreads = group.addResource("threads");
   const groupThreadsMethods = ["GET", "OPTIONS"];
   for (const method of groupThreadsMethods) {
-    groupThreads.addMethod(method, new Apigateway.LambdaIntegration(lambda));
+    groupThreads.addMethod(
+      method,
+      new Apigateway.LambdaIntegration(groupLambda)
+    );
   }
 
   // /groups/{groupId}/threads/latest  get latest thread in specific group
-  const groupThreadsLatest = group.addResource("threads/latest");
+  const groupThreadsLatest = groupThreads.addResource("latest");
   const groupThreadsLatestMethods = ["GET", "OPTIONS"];
   for (const method of groupThreadsLatestMethods) {
     groupThreadsLatest.addMethod(
       method,
-      new Apigateway.LambdaIntegration(lambda)
+      new Apigateway.LambdaIntegration(groupLambda)
     );
   }
 
   // /threads
-  const threadResource = api.root.addResource("threads");
+  const threadResource = threadApi.root.addResource("threads");
   const threadResourceMethods = ["POST", "OPTIONS"];
   for (const method of threadResourceMethods) {
-    threadResource.addMethod(method, new Apigateway.LambdaIntegration(lambda));
+    threadResource.addMethod(
+      method,
+      new Apigateway.LambdaIntegration(threadLambda)
+    );
   }
 
   // /threads/{threadId}?limit=v1&groupId=v2
   const thread = threadResource.addResource("{threadId}");
   const threadMethods = ["GET", "OPTIONS"];
   for (const method of threadMethods) {
-    thread.addMethod(method, new Apigateway.LambdaIntegration(lambda));
+    thread.addMethod(method, new Apigateway.LambdaIntegration(threadLambda));
   }
 
   new Cdk.CfnOutput(stack, "ChatApiUrl", {
-    value: api.url,
+    value: userApi.url,
     description: "Chat API URL",
   });
 
